36ca8a434d6b435bf2ad35c27f8858e59fcca694
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-09-09 10:01:37 +0100

    update

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,3 +1,4 @@
+import {SignedIn} from "@clerk/nextjs"
 import Link from "next/link";
 import Image from "next/image";
 
@@ -5,10 +6,17 @@ function Topbar(){
     return (
     <nav className="topbar">
         <Link href="/" className="flex items-center gap-4">
-            <Image src="/assets/logo.svg" alt="logo" width={28} height={28}/>
+            <Image src="/assets/golden-star.png" alt="logo" width={28} height={28}/>
             <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
         </Link>
+ 
+ <div className="flex items-center gap-1"> 
+    <div className="block md:hidden">
+        <SignedIn></SignedIn>
 
+        {}
+    </div>
+ </div>
     </nav>
     )
 }
