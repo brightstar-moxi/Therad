237e90e5c376ec33b1096ed3c36c062b7d355d8d
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-09-09 10:09:58 +0100

    update

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,8 +1,10 @@
-import {SignedIn} from "@clerk/nextjs"
+import {SignedIn, SignOutButton} from "@clerk/nextjs"
 import Link from "next/link";
 import Image from "next/image";
 
 function Topbar(){
+const isUserLoggedIn = true;
+
     return (
     <nav className="topbar">
         <Link href="/" className="flex items-center gap-4">
@@ -12,9 +14,21 @@ function Topbar(){
  
  <div className="flex items-center gap-1"> 
     <div className="block md:hidden">
-        <SignedIn></SignedIn>
+        <SignedIn>
+            <SignOutButton>
+                <div className="flex cursor-pointer">
+                    <Image src="/assets/logout.svg" alt="logout" width={2}>
 
-        {}
+                    </Image>
+                </div>
+            </SignOutButton>
+        </SignedIn>
+
+        {/* {isUserLoggedIn ? (
+            <div></div>
+        ) : (
+            <div></div>
+        )} */}
     </div>
  </div>
     </nav>
